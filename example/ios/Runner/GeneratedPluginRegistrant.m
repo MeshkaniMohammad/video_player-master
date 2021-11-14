65910aa36e8022c8227edb961d4f5cc29d668396
Mohammad Meshkani <mohammad.meshkani75@gmail.com>
2021-11-14 15:26:06 +0330

    changing names

--- example/ios/Runner/GeneratedPluginRegistrant.m
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -2,13 +2,9 @@
 //  Generated file. Do not edit.
 //
 
-#import "GeneratedPluginRegistrant.h"
+// clang-format off
 
-#if __has_include(<video_player/FLTVideoPlayerPlugin.h>)
-#import <video_player/FLTVideoPlayerPlugin.h>
-#else
-@import video_player;
-#endif
+#import "GeneratedPluginRegistrant.h"
 
 #if __has_include(<e2e/E2EPlugin.h>)
 #import <e2e/E2EPlugin.h>
@@ -16,11 +12,17 @@
 @import e2e;
 #endif
 
+#if __has_include(<video_player_x/FLTVideoPlayerPlugin.h>)
+#import <video_player_x/FLTVideoPlayerPlugin.h>
+#else
+@import video_player_x;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [FLTVideoPlayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTVideoPlayerPlugin"]];
   [E2EPlugin registerWithRegistrar:[registry registrarForPlugin:@"E2EPlugin"]];
+  [FLTVideoPlayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTVideoPlayerPlugin"]];
 }
 
 @end
